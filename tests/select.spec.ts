94018b23eee2f63973d7247a133b70538e1d498b
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2022-04-19 16:40:04 +0300

    fixed tests and deps

--- tests/select.spec.ts
+++ tests/select.spec.ts
@@ -21,40 +21,42 @@ class Aggregated {
 describe('select', () => {
   let service: IEntityService<Model, 'id', 'id', never>;
   const compiler = new TestCompiler();
+  let driver!: TestDriver;
   const all: any = [{id: 1, value: 'test'}, {id: 2, value: 'other'}, {id: 3, value: 'other'}, {id: 4, value: 'other'}] as Model[];
+  const aggregated: any = [{id: 1, count: 1}, {id: 2, count: 2}] as Aggregated[];
 
   beforeAll(() => {
-    const driver = new TestDriver();
+    driver = new TestDriver();
     const uow = new UnitOfWork(driver);
 
     service = new EntityService(Model, uow, compiler) as any;
   });
 
   it('query all', async () => {
-    compiler.mapper = () => all;
+    driver.mapper = () => all;
 
     const result = await service.select().where(x => x.id.eq(5)).fetch()
     expect(result).toEqual(all);
   });
 
   it('query one', async () => {
-    compiler.mapper = () => all;
+    driver.mapper = () => all;
 
     const result = await service.select().where(({id}) => id.eq(2)).fetchOne();
     expect(result).toEqual(all[0]);
   });
 
   it('query all aggregated', async () => {
-    compiler.mapper = () => all;
+    driver.mapper = () => aggregated;
 
     const result = await service.select().where(x => x.id.eq(5)).fetch(Aggregated)
-    expect(result).toEqual(all);
+    expect(result).toEqual(aggregated);
   });
 
   it('query one aggregated', async () => {
-    compiler.mapper = () => all;
+    driver.mapper = () => aggregated;
 
     const result = await service.select().where(({id}) => id.eq(2)).fetchOne(Aggregated);
-    expect(result).toEqual(all[0]);
+    expect(result).toEqual(aggregated[0]);
   });
 });
